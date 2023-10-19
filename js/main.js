7d59d3ca9dac318b8917118c0727a03d1a58e47c
Daniil Savchik <savchik.official@gmail.com>
2023-10-19 16:58:18 +0300

    Ready website with adaptive layout

---
+++ js/main.js
@@ -0,0 +1,52 @@
+"use strict";
+
+
+const btnDarkMode = document.querySelector('.dark-mode-btn');
+
+// 1. Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+    localStorage.setItem('darkMode', 'dark');
+}
+
+// 2. Проверка темной темы в localStorage
+
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add('dark-mode-btn--active');
+    document.body.classList.add('dark');
+} else if (localStorage.getItem('darkMode') === 'light') {
+    btnDarkMode.classList.remove('dark-mode-btn--active');
+    document.body.classList.remove('dark');
+}
+
+
+
+//Если меняются системные настройки, то меняем тему
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change', () => {
+    const newColorScheme = event.matches ? "dark" : "light";
+
+    if (newColorScheme === 'dark') {
+        btnDarkMode.classList.add('dark-mode-btn--active');
+        document.body.classList.add('dark');
+        localStorage.setItem('darkMode', 'dark');
+    } else {
+        btnDarkMode.classList.remove('dark-mode-btn--active');
+        document.body.classList.remove('dark');
+        localStorage.setItem('darkMode', 'light');
+    }
+});
+
+//Включение ночного режима по кнопке
+
+btnDarkMode.addEventListener('click', () => {
+    btnDarkMode.classList.toggle('dark-mode-btn--active');
+    const isDark = document.body.classList.toggle('dark');
+
+    if (isDark) {
+        localStorage.setItem('darkMode', 'dark')
+    } else {
+        localStorage.setItem('darkMode', 'light');
+    }
+})
